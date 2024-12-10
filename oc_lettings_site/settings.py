bc7670ed5dd95ac2b26475fc8c683ee7764b80b9
Afudu <abdoulseydou@hotmail.com>
2024-12-10 21:13:01 +0000

    Installed whitenoise

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'oc_lettings_site.urls'
@@ -68,6 +69,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 # Directory for static files during development
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+
+# Enable gzip and other optimizations
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 TEMPLATES = [
