acc8e92f8dae83172b7db5519731db02dfad3838
Afudu <abdoulseydou@hotmail.com>
2024-12-10 20:08:47 +0000

    Update static settings

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -10,3 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sentry-debug/', views.trigger_error, name='trigger_error'),
 ]
+
+# Serving static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
