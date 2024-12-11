4b7543a098d5ecff874252c291fd39aaf7d35cda
Afudu <abdoulseydou@hotmail.com>
2024-12-11 08:07:47 +0000

    update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -147,18 +147,18 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called
-    # `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    STORAGES = {
-        # ...
-        "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-        },
-    }
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-    WHITENOISE_MANIFEST_STRICT = False
+# if not DEBUG:
+#     # Tell Django to copy static assets into a path called
+#     # `staticfiles` (this is specific to Render)
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+#     # and renames the files with unique names for each version to support long-term caching
+#     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#     STORAGES = {
+#         # ...
+#         "staticfiles": {
+#             "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+#         },
+#     }
+#     STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+#     WHITENOISE_MANIFEST_STRICT = False
