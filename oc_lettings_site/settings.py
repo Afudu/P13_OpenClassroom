bdf8b3a248e525551feec59ceb64d1b674dba204
Afudu <abdoulseydou@hotmail.com>
2024-12-11 01:07:01 +0000

    update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -126,6 +126,7 @@ STORAGES = {
     },
 }
 
+WHITENOISE_MANIFEST_STRICT = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -146,5 +147,3 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-WHITENOISE_MANIFEST_STRICT = False
