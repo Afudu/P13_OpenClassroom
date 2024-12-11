d43a1582a657915ee1a9ceb9db3673a757f039df
Afudu <abdoulseydou@hotmail.com>
2024-12-11 00:38:56 +0000

    update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -139,3 +139,5 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+WHITENOISE_MANIFEST_STRICT = False
